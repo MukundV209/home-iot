d0a934af26ea125301f46c46a05c08090be71c99
Mukund <v.mukund200592@gmail.com>
2024-09-23 12:21:07 +0530

    Added cred.py file

---
+++ cred.py
@@ -0,0 +1,6 @@
+DEVICE_ID = "your esp32 device id"
+CLOUD_PASSWORD = "your cloud key for the device"
+WIFI_SSID = "your_ssid"
+WIFI_PASSWORD = "your_password"
+GITHUB_URL = "https://raw.githubusercontent.com/username/repository/branch/path/to/action.py"
+# copy link by opening the raw page of action.py file
