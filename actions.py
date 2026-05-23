5f17342e8185d2ace734a63460c9c99acf864b1d
MukundV209 <v.mukund200592@gmail.com>
2026-05-23 18:58:51 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,14 +1,9 @@
-from machine import Pin
 from hcsr04 import HCSR04
 from time import sleep
 
-led = Pin(2,Pin.OUT)
-led.value(1)
 sensor = HCSR04(trigger_pin=12, echo_pin=14)
 
 while True:
-    led.value(0)
     distance = sensor.distance_cm()
     print('Distance:', distance, 'cm')
     sleep(1)
-    led.value(1)
